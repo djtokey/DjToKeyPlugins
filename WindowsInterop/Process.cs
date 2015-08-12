293c80442206f634cfc30ca4341cf631cdd80d70
Marcin Badurowicz <m@badurowicz.net>
2015-08-12 16:37:47 +0200

    second available class in the plugin for starting processes

---
+++ WindowsInterop/Process.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Ktos.DjToKey.Plugins.Contracts;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Ktos.DjToKey.Plugins.WindowsInterop
+{
+    class Process : IScriptObject
+    {
+        private const string objName = "Process";
+
+        public string Name
+        {
+            get
+            {
+                return objName;
+            }
+        }
+
+        public object Object
+        {
+            get
+            {
+                return w;
+            }
+        }
+
+        private ProcessImpl w;
+
+        public Process()
+        {
+            w = new ProcessImpl();
+        }
+    }
+
+    class ProcessImpl
+    {
+        public void Start(string path)
+        {
+            System.Diagnostics.Process.Start(path);
+        }
+    }
+}
--- WindowsInterop/WindowsInterop.cs
+++ WindowsInterop/WindowsInterop.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using Ktos.DjToKey.Plugins.Contracts;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Ktos.DjToKey.Plugins.WindowsInterop
 {
     class WindowsInterop : IScriptObject
     {
+        private const string objName = "Windows";
+
         public string Name
         {
             get
             {
-                return "Windows";
+                return objName;
             }
         }
 
@@ -21,11 +25,37 @@ namespace Ktos.DjToKey.Plugins.WindowsInterop
             }
         }
 
-        private Windows w;
+        private WindowsImpl w;
 
         public WindowsInterop()
         {
-            w = new Windows();
+            w = new WindowsImpl();
+        }
+    }
+
+    class WindowsImpl
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        /// <summary>
+        /// Returns title of active window in operating system
+        /// </summary>
+        /// <returns>Title of foreground window</returns>
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
         }
     }
 }
