23b83780dddb39693651eed98da562e4b8e2c271
Marcin Badurowicz <m@badurowicz.net>
2015-08-12 16:33:45 +0200

    a plugin allowing to interoperate with Windows operating system

---
+++ WindowsInterop/WindowsInterop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Ktos.DjToKey.Plugins.Contracts;
+
+namespace Ktos.DjToKey.Plugins.WindowsInterop
+{
+    class WindowsInterop : IScriptObject
+    {
+        public string Name
+        {
+            get
+            {
+                return "Windows";
+            }
+        }
+
+        public object Object
+        {
+            get
+            {
+                return w;
+            }
+        }
+
+        private Windows w;
+
+        public WindowsInterop()
+        {
+            w = new Windows();
+        }
+    }
+}
